273e10cbdf6ac7c9bce9a2bb2082eeda838dd361
Saad Labied <saad.labied.95@gmail.com>
2022-01-16 22:15:01 +0100

    add server adress

--- scripts/ci-deploy.sh
+++ scripts/ci-deploy.sh
@@ -16,6 +16,8 @@ mv ./kube/do-sample-deployment.yml.out ./kube/do-sample-deployment.yml
 echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
 
 ./kubectl \
+  --kubeconfig=/dev/null \
+  --server=$KUBERNETES_SERVER \
   --certificate-authority=cert.crt \
   --token=$KUBERNETES_TOKEN \
   apply -f ./kube/
